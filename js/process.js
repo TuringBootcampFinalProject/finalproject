9c4f9097856b2c806d640c3710904edeac6bf1c8
Zoltan Puha <puha.zoli@gmail.com>
2016-11-29 12:28:31 +0100

    only 5

--- js/process.js
+++ js/process.js
@@ -2,7 +2,7 @@ theMovieDb.common.api_key = "96e769aaf4e0ffe7112b1cc2cbdd9db9";
 
 function successCB(data) {
     var obj = $.parseJSON(data);
-    obj.results.forEach(function(item){
+    obj.results.slice(0,4).forEach(function(item){
     	var url = theMovieDb.common.images_uri + item.poster_path;
     	var img = $(document.createElement("img"));
       	img.attr('src', url);
